cf912220698b193f92293c4b7c66550de7d92837
Chrinovic Mukanya <164991237+ChrinovicMukanya@users.noreply.github.com>
2024-05-06 12:07:25 +0200

    Add files via upload
    Program that calculates the total sum of values in the rows and total sum of values in the column in C

---
+++ arrayTotals.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	int array[5][5] = {
+			   {1,2,3,4,5},
+			   {6,7,8,9,10},
+			   {11,12,13,14,15},
+			   {16,17,18,19,20},
+			   {21,22,23,24,25}
+	};
+	int x;
+	int y;
+
+	int rowSum = 0;
+	int columnSum = 0;
+
+	//Row Total
+	for(x = 0; x < 5; ++x){
+		for (y = 0; y < 5; ++y){
+			rowSum += array[x][y];
+		}	
+	}
+
+	//Column Total
+	for(x = 0; x < 5; ++x){ 
+		for(y = 0; y < 5; ++y){
+			columnSum += array[y][x];
+		}
+	}
+
+	printf("Row Total %d\n", rowSum);
+	printf("Column Total = %d\n", columnSum);
+}
