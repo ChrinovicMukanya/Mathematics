c1760dedc8e2e38cc8cd5b4f9d29335c28123913
Chrinovic Mukannya <chrinovicmu@gmail.com>
2024-05-11 22:56:19 +0200

    Multiples of number C code

---
+++ multiplesOfProgram.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+	int numberLimit = strtol(argv[1], NULL, 10);
+	const int multipleOf = strtol(argv[2], NULL, 10);
+
+	int x = 0;
+	while (x < numberLimit - 1){
+		if (( x % multipleOf) == 0){
+			printf("%d\n", x);
+			++x;
+		}
+	}
+}
