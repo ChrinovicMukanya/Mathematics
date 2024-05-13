06c1fbd9624247d53b4fff34e7fc4d19b4f858b0
Chrinovic Mukanya <chrinovicmu@gmail.com>
2024-05-13 23:11:26 +0200

    Add files via upload

---
+++ determinant_of_2Dmatrix.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+	int array[2][2] = {
+		{3,2,},
+		{2,1,}
+	};
+
+	int determinant = (array[0][0] * array[1][1]) - (array[0][1] * array[1][0]);
+	printf("%d\n", determinant);
+
+
+
+}
