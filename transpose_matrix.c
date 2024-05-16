688418c127f88df25c4f041be883accf9959ea41
Chrinovic Mukanya <chrinovicmu@gmail.com>
2024-05-16 16:24:13 +0200

    Update transpose_matrix.c

--- transpose_matrix.c
+++ transpose_matrix.c
@@ -9,16 +9,20 @@ int main(int argc, char * argv[])
 		{3, 1, -2},
 		{1, 0, 4}
 	};
+	
 	int x;
 	int y;
 	printf("ORIGINAL MATRIX\n");
+	
 	for (x = 0; x < 3; ++x){
 		for (y = 0; y < 3; ++y){
 			printf("%d\n", matrix_array[x][y]);
 		}
 	}
+	
 	printf("\n\n");
 	printf("TRANSPOSE MATRIX\n");
+	
 	int new_transpose_matrix[3][3];
 		for (x = 0; x < 3; ++x){
 			for (y = 0; y < 3; ++y){
