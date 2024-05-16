93513a72b63bc31bd6f6f67ea95305a1407b10e6
Chrinovic Mukanya <chrinovicmu@gmail.com>
2024-05-16 16:21:46 +0200

    Add files via upload
    C Implementation of matrix transposition

---
+++ transpose_matrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * arhv[])
+{
+	int matrix_array[3][3] = {
+
+		{2, 1, -1},
+		{3, 1, -2},
+		{1, 0, 4}
+	};
+	int totalElements = 3 * 3;
+	int arraylength = sizeof(totalElements); 
+	int x;
+	int y;
+	printf("ORIGINAL MATRIX\n");
+	for (x = 0; x < 3; ++x){
+		for (y = 0; y < 3; ++y){
+			printf("%d\n", matrix_array[x][y]);
+		}
+	}
+	printf("\n\n");
+	printf("TRANSPOSE MATRIX\n");
+	int new_transpose_matrix[3][3];
+		for (x = 0; x < 3; ++x){
+			for (y = 0; y < 3; ++y){
+				new_transpose_matrix[x][y] = matrix_array[y][x];
+				printf("%d\n", new_transpose_matrix[x][y]);
+			}
+		}
+
+}
